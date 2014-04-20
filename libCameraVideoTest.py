31aae7524b240a4451fb9d80c0e46f8ed76391d6
Junior Grégoire <junior.gregoire@gmail.com>
2014-04-19 20:06:39 -0400

    add test files

---
+++ libCameraVideoTest.py
@@ -0,0 +1,26 @@
+
+import cv2
+import numpy
+import camera
+import threading
+
+import time
+
+cam = camera.Camera()
+cam.start()
+img = cam.getCam()	
+
+
+time.sleep(1)
+
+while True:
+	t1=time.time()	
+	frameId = cam.getFrame()	
+	cv2.imshow("Video",cv2.resize(img[frameId],(400,300)))
+	cv2.cv.WaitKey(10)
+	#time.sleep(10);
+	t2=time.time()
+	fps = 1/(t2-t1)
+	#print "fps:"+str(fps)
+	
+
---
+++ mainTest.cpp
@@ -0,0 +1,10 @@
+#include "camera.h"
+
+int main()
+{
+    _import_array();
+    Camera cam;
+    cam.initialize();
+    cam.start();
+   // cam.startVideo();
+}
